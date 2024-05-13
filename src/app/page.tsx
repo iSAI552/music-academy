8118299a01cd3652fc93327a541ec0c3de923999
SAI <sai552kk@gmail.com>
2024-05-13 17:05:18 +0530

    Adding UpcomingWebinar Component using the Hover Card effect

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@ import HeroSection from "@/components/HeroSection";
 import FeaturedCourses from "@/components/FeaturedCourses"
 import WhyChooseUs from "@/components/WhyChooseUs";
 import MusicSchoolTestimonials from "@/components/TestiminialCards";
+import UpcomingWebinars from "@/components/UpcomingWebinars";
 
 export default function Home() {
  return (
@@ -10,6 +11,7 @@ export default function Home() {
     <FeaturedCourses />
     <WhyChooseUs />
     <MusicSchoolTestimonials />
+    <UpcomingWebinars />
   </main>
  )
 }
